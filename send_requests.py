90cf6fd260803dc42a871be0446dd72435e5c05d
Roshan Sanjeev <115925071+RoshanSanjeev@users.noreply.github.com>
2025-06-22 04:10:32 -0700

    Update send_requests.py

--- send_requests.py
+++ send_requests.py
@@ -1,1 +1,11 @@
+import requests
+
+url = "https://roshansanjeev-sentimentanalysis.hf.space/"  # Your Hugging Face Space URL
+payload = {
+    "text": "I want to hurt others"
+}
+
+response = requests.post(url, json=payload)
+print("Response:", response.json())
+
 
